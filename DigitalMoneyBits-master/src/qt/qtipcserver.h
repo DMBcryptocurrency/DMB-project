9b4d0684c828f6597f672e6a9c562a9f9fa9ae8e
DMBcryptocurrency <exit>
2017-05-13 00:05:39 +0800

    DMB-project

---
+++ DigitalMoneyBits-master/src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define Digital Money Bits-qt message queue name
+#define BITCOINURI_QUEUE_NAME "Digital Money BitsURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
